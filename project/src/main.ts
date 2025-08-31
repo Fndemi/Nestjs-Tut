45f953e061e3d86004024ec82ca10d7c79e82191
Florence Ndemi <ndemiflorence@gmail.com>
2025-08-31 11:12:00 +0300

    Added rate limiting ,did migrations and tested API

--- project/src/main.ts
+++ project/src/main.ts
@@ -4,6 +4,8 @@ import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
